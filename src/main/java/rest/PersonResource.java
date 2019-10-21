35b0e514b71815e12dfff3b0ef4863a5d77808db
Dennis <drh89@live.com>
2019-10-21 11:47:52 +0200

    remove pop

--- src/main/java/rest/PersonResource.java
+++ src/main/java/rest/PersonResource.java
@@ -102,30 +102,30 @@ public class PersonResource {
         return Response.ok().entity(GSON.toJson(FACADE.editPerson(p))).build();
     }
 
-    @GET
-    @Path("/pop")
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
-    public String populate() {
-        PersonDto first = new PersonDto("Jens", "Hansen", "test@test.dk", "Football", "Teamsport where you kick to a ball", "11111111", "Jens Hansens Phone", "Hjalmersgade", "22", "2tv", 2200, "Copenhagen");
-        PersonDto second = new PersonDto("Hanne", "Hansen", "test2@test.dk", "Handball", "Teamsport where you throw a ball", "66666666", "Hanne Hansens Phone", "Hjalmersgade", "22", "2tv", 2200, "Copenhagen");
-        PersonDto third = new PersonDto("Bjarne", "Lund", "test3@test.dk", "Football", "Teamsport where you kick to a ball", "66233344", "Bjarne Lunds Phone", "Lortegade", "232", "3tv", 2200, "Copenhagen");
-        PersonDto fourth = new PersonDto("Hans", "Berg", "test4@test.dk", "Volleyball", "Teamsport with a ball", "22222222", "Hans Bergs Phone", "Jomfruegade", "2", "1tv", 8210, "Aarhus");
-        PersonDto fifth = new PersonDto("Ken", "Nielsen", "test5@test.dk", "Cycling", "Cardio Training", "33333333", "Ken Nielsens Phone", "Abegade", "99", "22mdtf", 9210, "Aalborg");
-        PersonDto sixth = new PersonDto("Julie", "Olsen", "test6@test.dk", "Shopping", "Spending money", "44444444", "Julie Olsens Phone", "Borupsvej", "18", "5th", 2300, "Copenhagen");
-        
-        
-        first.getHobbies().add(new HobbyDto(new Hobby("Curling", "Boring game")));
-        second.getHobbies().add(new HobbyDto(new Hobby("Running", "Cardio training")));
-//        sixth.getPhones().add(new PhoneDto(new Phone("99999999", "Julie Olsens work phone")));
-        FACADE.addPerson(first);
-        FACADE.addPerson(second);
-        FACADE.addPerson(third);
-        FACADE.addPerson(fourth);
-        FACADE.addPerson(fifth);
-        FACADE.addPerson(sixth);
-        
-        return "Success!";
-    }
+//    @GET
+//    @Path("/pop")
+//    @Produces(MediaType.APPLICATION_JSON)
+//    @Consumes(MediaType.APPLICATION_JSON)
+//    public String populate() {
+//        PersonDto first = new PersonDto("Jens", "Hansen", "test@test.dk", "Football", "Teamsport where you kick to a ball", "11111111", "Jens Hansens Phone", "Hjalmersgade", "22", "2tv", 2200, "Copenhagen");
+//        PersonDto second = new PersonDto("Hanne", "Hansen", "test2@test.dk", "Handball", "Teamsport where you throw a ball", "66666666", "Hanne Hansens Phone", "Hjalmersgade", "22", "2tv", 2200, "Copenhagen");
+//        PersonDto third = new PersonDto("Bjarne", "Lund", "test3@test.dk", "Football", "Teamsport where you kick to a ball", "66233344", "Bjarne Lunds Phone", "Lortegade", "232", "3tv", 2200, "Copenhagen");
+//        PersonDto fourth = new PersonDto("Hans", "Berg", "test4@test.dk", "Volleyball", "Teamsport with a ball", "22222222", "Hans Bergs Phone", "Jomfruegade", "2", "1tv", 8210, "Aarhus");
+//        PersonDto fifth = new PersonDto("Ken", "Nielsen", "test5@test.dk", "Cycling", "Cardio Training", "33333333", "Ken Nielsens Phone", "Abegade", "99", "22mdtf", 9210, "Aalborg");
+//        PersonDto sixth = new PersonDto("Julie", "Olsen", "test6@test.dk", "Shopping", "Spending money", "44444444", "Julie Olsens Phone", "Borupsvej", "18", "5th", 2300, "Copenhagen");
+//        
+//        
+//        first.getHobbies().add(new HobbyDto(new Hobby("Curling", "Boring game")));
+//        second.getHobbies().add(new HobbyDto(new Hobby("Running", "Cardio training")));
+////        sixth.getPhones().add(new PhoneDto(new Phone("99999999", "Julie Olsens work phone")));
+//        FACADE.addPerson(first);
+//        FACADE.addPerson(second);
+//        FACADE.addPerson(third);
+//        FACADE.addPerson(fourth);
+//        FACADE.addPerson(fifth);
+//        FACADE.addPerson(sixth);
+//        
+//        return "Success!";
+//    }
 
 }
